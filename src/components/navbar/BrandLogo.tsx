3fe9e48d651ddcf2db3d678434f4931f042380f4
shafin90 <farjanashanta89@gmail.com>
2026-09-13 16:42:07 +0600

    feat: remove favicon mark from navbar brand

--- src/components/navbar/BrandLogo.tsx
+++ src/components/navbar/BrandLogo.tsx
@@ -6,8 +6,7 @@ type BrandLogoProps = {
 
 function BrandLogo({ className = '' }: BrandLogoProps) {
   return (
-    <a href="#" className={`flex items-center gap-2 ${className}`}>
-      <img src="/favicon.svg" alt="Dev Stack logo" className="h-9 w-9" />
+    <a href="#" className={`flex items-center ${className}`}>
       <img src={logoText} alt="Dev Stack" className="h-9 w-auto" />
     </a>
   )
